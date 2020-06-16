1d931683748b951613122d06633ea8c7e371787f
gabriel <gabrielalmeidads@gmail.com>
2020-06-15 22:58:35 -0300

    Separação de rotas, configurações e conexões

--- src/server.js
+++ src/server.js
@@ -1,99 +1,14 @@
-const express = require("express")
-const server = express()
+/*****CONFIGURAÇÕES*****/
+require('dotenv/config')
+const server = require('./config/serverConfig')
+const routes = require('express').Router()
 
-//pegar o banco de dados
-//const db = require("./database/db.js")
+/*****BANCO DE DADOS*****/
+//const db = require('./database/mongoDB-connection')
 
-//configurar pasta pública
-server.use(express.static("public"))
+/*****ROTAS*****/
+server.use('/', require('./routes/index'))
 
-//habilitar o uso do req.body na nossa aplicação
-server.use(express.urlencoded({extended: true}))
-
-//utilizando template engine
-const nunjucks = require("nunjucks")
-nunjucks.configure("src/views", {
-    express: server,
-    noCache: true
-})
-
-
-
-
-//configurar caminhos|rotas da minha aplicação
-//página inicial
-//req: Requisição
-//res: Respostas
-//get configuração de rota
-server.get("/", (req, res) => {
-    return res.render("index.html")
-})
-
-server.get("/create-point", (req, res) => {
-
-    //req.query: Query String da nossa url
-    //req.query()
-
-    return res.render("create-point.html")
-})
-
-server.post("/savepoint", (req, res) => {
-    //req.body: O corpo do nosso formulário
-
-    //inserir dados no banco de dados
-    const query = `INSERT INTO places (
-        image,
-        name,
-        address,
-        address2,
-        state,
-        city,
-        items
-    ) VALUES (?,?,?,?,?,?,?);`
-
-    const values = [
-        req.body.image,
-        req.body.name,
-        req.body.address,
-        req.body.address2,
-        req.body.state,
-        req.body.city,
-        req.body.items
-    ]
-
-    function afterInsertData(err){
-        if (err){
-            console.log(err)
-            return res.send("Erro no cadastro!")
-        }
-        return res.render("create-point.html", {saved: true})
-    }
-
-    db.run(query, values, afterInsertData)
-    
-})
-
-
-
-server.get("/search", (req, res) => {
-
-    const search = req.query.search
-
-    if (search == ""){
-        //pesquisa vazia
-        return res.render("search-results.html", {total: 0})
-    } 
-
-    //pegar os dados do banco de dados
-    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
-        if (err){
-            return console.log(err)
-        }
-        const total = rows.length
-        //mostrar a página html com os dados do banco de dados
-        return res.render("search-results.html", {places: rows, total: total})
-    })
-})
 
 //ligar o servidor
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT)
